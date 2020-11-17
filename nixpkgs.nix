797eebb3e1bea84857cf0acccc29e649bc48dac9
Matthew Brisebois <matthew@webheroes.ca>
2020-11-17 16:07:36 -0700

    Add nix files so that lair-keystore binary is in nix-shell

---
+++ nixpkgs.nix
@@ -0,0 +1,4 @@
+import (fetchTarball {
+  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/8b51eda9384d55a1ced27e7326dfa0e6125dc02c.tar.gz";
+  sha256 = "0gzyxafvs4wkn0hx8v01bwk3najz8fqf9drqlvmx91k2lrzj8xxc";
+})
