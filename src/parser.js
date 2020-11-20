c760b1a5356a9ba669e14996e9e749b9c0ed174b
Matthew Brisebois <matthew@webheroes.ca>
2020-11-20 15:35:33 -0700

    Reimplement parser so it is based of streams.Duplex o fix failing parser error

--- src/parser.js
+++ src/parser.js
@@ -1,6 +1,6 @@
 const path				= require('path');
 const log				= require('@whi/stdlog')(path.basename( __filename ), {
-    level: process.env.LOG_LEVEL || 'silly',
+    level: process.env.LOG_LEVEL || 'fatal',
 });
 
 const stream				= require('stream');
@@ -8,114 +8,160 @@ const {
     ParserError,
 }					= require('./error.js');;
 
+const delay				= (ms) => new Promise(f => setTimeout(f, ms));
+
 
 const MSG_LEN_SIZE			= 4;
 const WIRE_TYPE_SIZE			= 4;
 const MSG_ID_SIZE			= 8;
 const HEADER_SIZE			= MSG_LEN_SIZE + WIRE_TYPE_SIZE + MSG_ID_SIZE;
 
-class Parser {
-    constructor ( msg_processor_callback ) {
+const STATE_GET_HEADER			= 0;
+const STATE_GET_PAYLOAD			= 1;
+
+class MessageParser extends stream.Duplex {
+    constructor () {
+	super()
+
 	this.msg_count			= 0;
-	this.partial_header		= null;
-	this.partial_package		= null;
+
+	this.state			= STATE_GET_HEADER;
+
+	this.need_more_buffer		= true;
+	this._bufferedBytes		= 0;
+	this._buffers			= [];
+	this._payloadLength		= 0;
+	this.current_msg		= null;
+	this.next_payload		= null;
     }
 
-    // Reliable stream parsing with support for pass-through forwarding
-    //
-    // - If: Header exists
-    //   - If: Header is pass-through, forward bytes until message length reached
-    //   - Else: consume body for Header
-    // - Else if: buffer size > header size, parse Header
-    //   - If: Wire Type or Request is for shim, continue
-    //   - Else: mark header as pass-through and begin forwarding bytes
-    // - Else: wait for next buffer
-    //
-    consume ( buf ) {
+    _write ( buf, encoding, cb ) {
 	if ( ! (buf instanceof Uint8Array) )
 	    throw new ParserError(`Cannot consume type '${typeof buf}'`);
-	console.log("Incoming", buf.length, buf );
 
-	// Finish off any previous package that had missing bytes.
-	if ( this.partial_package !== null ) {
-	    let missing_bytes_length	= this.partial_package.header.length - this.partial_package.payload.length;
+	log.silly("Incoming chunk (%s): %s", buf.length, buf.toString('hex') );
+	this._bufferedBytes	       += buf.length;
+	this._buffers.push( buf );
+	this.need_more_buffer		= false;
 
-	    console.log("Waiting for", missing_bytes_length, "got", buf.length );
-	    if ( buf.length >= missing_bytes_length ) {
-		let header		= this.partial_package.header;
-		let payload		= Buffer.concat([ this.partial_package.payload, buf.slice( 0, missing_bytes_length ) ]);
+	this.processBuffers().catch(console.error);
 
-		this.next({ header, payload });
+	return cb() || true;
+    }
 
-		this.partial_package	= null;
-		buf			= buf.slice( missing_bytes_length );
-	    } else {
-		this.partial_package.payload = Buffer.concat([ this.partial_package.payload, buf ]);
-		return;
+    async processBuffers () {
+	await delay(0);
+
+	do {
+	    log.silly("State maching loop: %s (%s bytes avaiable)", this.state, this._bufferedBytes );
+	    switch ( this.state ) {
+	    case STATE_GET_HEADER:
+		this.parse_header();
+		break;
+	    case STATE_GET_PAYLOAD:
+		this.parse_payload();
+		break;
+	    default:
+		this.need_more_buffer	= true;
+		break;
 	    }
+	    log.info("End of loop state: %s (more? %s)", this.state, this.need_more_buffer );
+	} while (this.need_more_buffer === false);
+    }
+
+    consume ( n ) {
+	this._bufferedBytes -= n;
+
+	if (n === this._buffers[0].length) return this._buffers.shift();
+
+	if (n < this._buffers[0].length) {
+	    const buf = this._buffers[0];
+	    this._buffers[0] = buf.slice(n);
+	    return buf.slice(0, n);
 	}
 
+	const dst = Buffer.allocUnsafe(n);
 
-	let header_bytes;
+	do {
+	    const buf = this._buffers[0];
+	    const offset = dst.length - n;
 
-	console.log("Continued", buf );
-	if ( this.partial_header !== null ) {
-	    if ( (this.partial_header.length + buf.length) >= HEADER_SIZE ) {
-		let missing_header_bytes = HEADER_SIZE - this.partial_header.length;
-		header_bytes		= Buffer.concat([ this.partial_header, buf.slice( 0, missing_header_bytes ) ]);
-		buf			= buf.slice( missing_header_bytes );
-		this.partial_header	= null;
-		console.log("Fulfil partial", header_bytes, missing_header_bytes, buf );
+	    if (n >= buf.length) {
+		dst.set(this._buffers.shift(), offset);
 	    } else {
-		this.partial_header	= Buffer.concat([ this.partial_header, buf ]);
-		return;
+		dst.set(new Uint8Array(buf.buffer, buf.byteOffset, n), offset);
+		this._buffers[0] = buf.slice(n);
 	    }
-	} else if ( buf.length >= HEADER_SIZE ) {
-	    header_bytes		= buf.slice( 0, HEADER_SIZE );
-	    buf				= buf.slice( HEADER_SIZE );
-	} else {
-	    this.partial_header		= buf;
+
+	    n -= buf.length;
+	} while (n > 0);
+
+	return dst;
+    }
+
+    parse_header () {
+	if ( this._bufferedBytes < HEADER_SIZE ) {
+	    this.need_more_buffer	= true;
 	    return;
 	}
+	log.info("Parsing (%s) header from %s available bytes", HEADER_SIZE, this._bufferedBytes );
 
-	console.log("Header", header_bytes );
-	let Header = {
+	let header_bytes		= this.consume( HEADER_SIZE );
+
+	let payload_promise		= new Promise((f,r) => {
+	    this.next_payload		= payload => {
+		this.next_payload	= null;
+		f(payload);
+	    }
+	});
+
+	this.current_msg		= {
 	    "length":		header_bytes.readUInt32LE() - HEADER_SIZE,
 	    "wire_type":	header_bytes.readUInt32LE(4),
-	    "id":		header_bytes.readBigUInt64LE(8),
+	    "id":		header_bytes.slice(8).toString('hex'),
+	    "payload":		() => payload_promise,
 	};
 
-	if ( buf.length >= Header.length ) {
-	    let payload			= buf.slice( 0, Header.length );
-	    console.log("Complete package", Header, payload );
-	    this.next({
-		"header":	Header,
-		payload,
-	    });
+	this.emit('header', this.current_msg );
 
-	    buf				= buf.slice( Header.length );
-	}
-	else {
-	    this.partial_package	= {
-		"header":	Header,
-		"payload":	buf,
-	    };
-	}
+	this.state			= STATE_GET_PAYLOAD;
+	log.info("Change state to: %s", this.state );
+    }
 
-	return buf.length;
+    parse_payload () {
+	if ( this._bufferedBytes < this.current_msg.length ) {
+	    this.need_more_buffer	= true;
+	    return;
+	}
+	log.info("Parsing (%s) payload from %s available bytes", this.current_msg.length, this._bufferedBytes );
+
+	let payload_bytes		= this.consume( this.current_msg.length );
+
+	this.emit('payload', payload_bytes );
+	this.next_payload( payload_bytes );
+
+	this.state			= STATE_GET_HEADER;
+	this.current_msg		= null;
     }
 
     stop () {
 	this.stopped			= true;
     }
 
-    next ( v ) {
-	setTimeout( () => this.f(v), 0 );
-    }
-
     [Symbol.asyncIterator]() {
 	this.stopped			= false;
 	const $this			= this;
+
+	let parsed_headers		= [];
+	let nextHeader			= null;
+	this.on('header', header => {
+	    log.normal("Parsed a new header: %s", header );
+	    if ( nextHeader === null )
+		parsed_headers.push( header );
+	    else
+		nextHeader( header );
+	});
+
 	return {
 	    next() {
 		$this.msg_count++;
@@ -124,11 +170,14 @@ class Parser {
 		    return Promise.resolve({ done: true });
 
 		return new Promise((f,r) => {
+		    if ( parsed_headers.length )
+			return f({ "value":  parsed_headers.shift() });
+
 		    let toid		= setTimeout( f, 100, { value: null } );
 
-		    $this.f		= (v) => {
+		    nextHeader		= v => {
 			clearTimeout( toid );
-			$this.f		= null;
+			nextHeader	= null;
 			f({ "value": v });
 		    };
 		});
@@ -139,5 +188,5 @@ class Parser {
 
 
 module.exports = {
-    Parser,
+    MessageParser,
 };
--- tests/unit/test_parser.js
+++ tests/unit/test_parser.js
@@ -5,78 +5,78 @@ const log				= require('@whi/stdlog')(path.basename( __filename ), {
 
 const stream				= require('stream');
 const expect				= require('chai').expect;
-const { Parser }			= require('../../src/parser.js');
+const { MessageParser }			= require('../../src/parser.js');
 
 const delay				= (ms) => new Promise(f => setTimeout(f, ms));
 const compress				= str => str.replace(/ /g, "");
 
-const full_msg				= Buffer.from("00010000100000ff0100000000000000ae6dd17372ce0fba967ffe26ccf02cd8d7ce670028adc3c90346d4a1b2f22d7fc7e9d5ad8f4420d9e32bb3f55e89e9467f7d225427d9abca6832dc581f947faae00ea2cacb27ba07d10ecb231c16672c0246af177da578ae57dafbed8a6f268d7f50e0247182f2a753509c3e998efde7d1139d47a38b51fb24451e3060a148d404de0b29e436299cb85876875b588ee2c59fe659b5857bb427f0f8d87a6fcee87ccd97b10472d1a5d402be705c73b936746e52dedec1ae06c980413503befd9e126f00edc4df30725d693a842d1be87450a8bac987c7c9181363b6672c50302db4bf5b26ca2924005f8417c21ee50eb5", "hex");
+const full_msg				= "00010000100000ff0100000000000000ae6dd17372ce0fba967ffe26ccf02cd8d7ce670028adc3c90346d4a1b2f22d7fc7e9d5ad8f4420d9e32bb3f55e89e9467f7d225427d9abca6832dc581f947faae00ea2cacb27ba07d10ecb231c16672c0246af177da578ae57dafbed8a6f268d7f50e0247182f2a753509c3e998efde7d1139d47a38b51fb24451e3060a148d404de0b29e436299cb85876875b588ee2c59fe659b5857bb427f0f8d87a6fcee87ccd97b10472d1a5d402be705c73b936746e52dedec1ae06c980413503befd9e126f00edc4df30725d693a842d1be87450a8bac987c7c9181363b6672c50302db4bf5b26ca2924005f8417c21ee50eb5";
 
-const half1_msg				= Buffer.from("00010000100000ff0100000000000000ae6dd17372ce0fba967ffe26ccf02cd8d7ce670028adc3c90346d4a1b2f22d7fc7e9d5ad8f4420d9e32bb3f55e89e9467f7d225427d9abca6832", "hex");
-const half2_msg				= Buffer.from("dc581f947faae00ea2cacb27ba07d10ecb231c16672c0246af177da578ae57dafbed8a6f268d7f50e0247182f2a753509c3e998efde7d1139d47a38b51fb24451e3060a148d404de0b29e436299cb85876875b588ee2c59fe659b5857bb427f0f8d87a6fcee87ccd97b10472d1a5d402be705c73b936746e52dedec1ae06c980413503befd9e126f00edc4df30725d693a842d1be87450a8bac987c7c9181363b6672c50302db4bf5b26ca2924005f8417c21ee50eb5", "hex");
+const half1_msg				= "00010000100000ff0100000000000000ae6dd17372ce0fba967ffe26ccf02cd8d7ce670028adc3c90346d4a1b2f22d7fc7e9d5ad8f4420d9e32bb3f55e89e9467f7d225427d9abca6832";
+const half2_msg				= "dc581f947faae00ea2cacb27ba07d10ecb231c16672c0246af177da578ae57dafbed8a6f268d7f50e0247182f2a753509c3e998efde7d1139d47a38b51fb24451e3060a148d404de0b29e436299cb85876875b588ee2c59fe659b5857bb427f0f8d87a6fcee87ccd97b10472d1a5d402be705c73b936746e52dedec1ae06c980413503befd9e126f00edc4df30725d693a842d1be87450a8bac987c7c9181363b6672c50302db4bf5b26ca2924005f8417c21ee50eb5";
 
-const half1_head			= Buffer.from("00010000100000ff", "hex");
-const half2_head			= Buffer.from("0100000000000000ae6dd17372ce0fba967ffe26ccf02cd8d7ce670028adc3c90346d4a1b2f22d7fc7e9d5ad8f4420d9e32bb3f55e89e9467f7d225427d9abca6832dc581f94", "hex");
-const rest_of_payload			= Buffer.from("7faae00ea2cacb27ba07d10ecb231c16672c0246af177da578ae57dafbed8a6f268d7f50e0247182f2a753509c3e998efde7d1139d47a38b51fb24451e3060a148d404de0b29e436299cb85876875b588ee2c59fe659b5857bb427f0f8d87a6fcee87ccd97b10472d1a5d402be705c73b936746e52dedec1ae06c980413503befd9e126f00edc4df30725d693a842d1be87450a8bac987c7c9181363b6672c50302db4bf5b26ca2924005f8417c21ee50eb5", "hex");
+const half1_head			= "00010000100000ff";
+const half2_head			= "0100000000000000ae6dd17372ce0fba967ffe26ccf02cd8d7ce670028adc3c90346d4a1b2f22d7fc7e9d5ad8f4420d9e32bb3f55e89e9467f7d225427d9abca6832dc581f94";
+const rest_of_payload			= "7faae00ea2cacb27ba07d10ecb231c16672c0246af177da578ae57dafbed8a6f268d7f50e0247182f2a753509c3e998efde7d1139d47a38b51fb24451e3060a148d404de0b29e436299cb85876875b588ee2c59fe659b5857bb427f0f8d87a6fcee87ccd97b10472d1a5d402be705c73b936746e52dedec1ae06c980413503befd9e126f00edc4df30725d693a842d1be87450a8bac987c7c9181363b6672c50302db4bf5b26ca2924005f8417c21ee50eb5";
 
 
+
+function pipe_input_to_parser ( chunks, parser, interval = 10 ) {
+    const iid			= setInterval(() => {
+	log.info("Chunks remaining for parser: %s", chunks.length );
+	parser.write(  Buffer.from(chunks.shift(), "hex") );
+
+	if ( chunks.length === 0 )
+	    clearInterval( iid );
+    }, interval );
+}
 
 function parser_tests () {
     it("should parse chunk with a complete message", async () => {
-	const parser			= new Parser();
+	const parser			= new MessageParser();
 
-	parser.consume( full_msg );
+	parser.write( Buffer.from( full_msg, "hex" ) );
 
-	for await ( let LairPackage of parser ) {
-	    const { header,
-		    payload }		= LairPackage;
-
-	    expect( header.wire_type	).to.equal( 4278190096 );
-	    expect( payload		).to.have.length( 240 );
+	for await ( let header of parser ) {
+	    expect( header.wire_type		).to.equal( 4278190096 );
+	    expect( await header.payload()	).to.have.length( 240 );
 
 	    parser.stop();
 	}
     });
 
     it("should parse chunks with a partial messages", async () => {
-	const parser			= new Parser();
+	let chunks			= [ half1_msg, half2_msg ];
+	const parser			= new MessageParser();
+	pipe_input_to_parser( chunks, parser );
 
-	parser.consume( half1_msg );
-
-	for await ( let LairPackage of parser ) {
-	    if ( LairPackage === null ) {
-		parser.consume( half2_msg );
+	for await ( let header of parser ) {
+	    if ( header === null ) {
+		parser.write( chunks.shift() );
 		continue;
 	    }
 
-	    const { header,
-		    payload }		= LairPackage;
-
-	    expect( header.wire_type	).to.equal( 4278190096 );
-	    expect( payload		).to.have.length( 240 );
+	    expect( header.wire_type		).to.equal( 4278190096 );
+	    expect( await header.payload()	).to.have.length( 240 );
 
 	    parser.stop();
 	}
     });
 
     it("should parse chunks with a partial header message", async () => {
-	const parser			= new Parser();
+	let chunks			= [ half1_head, half2_head, rest_of_payload ];
+	const parser			= new MessageParser();
+	pipe_input_to_parser( chunks, parser );
 
-	parser.consume( half1_head );
-
-	let input			= [ half2_head, rest_of_payload ];
-
-	for await ( let LairPackage of parser ) {
-	    if ( LairPackage === null ) {
-		parser.consume( input.shift() );
+	for await ( let header of parser ) {
+	    if ( header === null ) {
+		expect( chunks.length		).to.be.gt( 0 );
+		parser.write( chunks.shift() );
 		continue;
 	    }
 
-	    const { header,
-		    payload }		= LairPackage;
-
-	    expect( header.wire_type	).to.equal( 4278190096 );
-	    expect( payload		).to.have.length( 240 );
+	    expect( header.wire_type		).to.equal( 4278190096 );
+	    expect( await header.payload()	).to.have.length( 240 );
 
 	    parser.stop();
 	}
@@ -91,27 +91,24 @@ function parser_tests () {
 	    "ae6dd17372ce0fbaae6dd17372ce0fba",
 	];
 
-	const parser			= new Parser();
+	const parser			= new MessageParser();
+	pipe_input_to_parser( chunks, parser );
 
 	let count			= 0;
-	for await ( let LairPackage of parser ) {
-	    if ( LairPackage === null ) {
-		expect( chunks.length		).to.be.gt( 0 );
-		parser.consume( Buffer.from( chunks.shift(), "hex" ) );
+	for await ( let header of parser ) {
+	    if ( header === null ) {
 		continue;
 	    }
 
-	    const { header,
-		    payload }		= LairPackage;
 	    count++;
 
 	    if ( count === 1 ) {
-		expect( header.wire_type	).to.equal( 4278190096 );
-		expect( payload			).to.have.length( 240 );
+		expect( header.wire_type		).to.equal( 4278190096 );
+		expect( await header.payload()		).to.have.length( 240 );
 		continue;
 	    }
 
-	    expect( payload			).to.have.length( 16 );
+	    expect( await header.payload()		).to.have.length( 16 );
 
 	    parser.stop();
 	}
@@ -122,26 +119,24 @@ function parser_tests () {
 	    "20000000100000ff0100000000000000ae6dd17372ce0fbaae6dd17372ce0fba20000000100000ff0100000000000000ae6dd17372ce0fbaae6dd17372ce0fba",
 	];
 
-	const parser			= new Parser();
+	const parser			= new MessageParser();
 
-	parser.consume( Buffer.from( chunks.shift(), "hex" ) );
+	parser.write( Buffer.from( chunks.shift(), "hex" ) );
 
 	let count			= 0;
-	for await ( let LairPackage of parser ) {
-	    if ( LairPackage === null )
+	for await ( let header of parser ) {
+	    if ( header === null )
 		throw new Error(`should not get here`);
 
-	    const { header,
-		    payload }		= LairPackage;
 	    count++;
 
 	    if ( count === 1 ) {
 		expect( header.wire_type	).to.equal( 4278190096 );
-		expect( payload			).to.have.length( 16 );
+		expect( await header.payload()	).to.have.length( 16 );
 		continue;
 	    }
 
-	    expect( payload			).to.have.length( 16 );
+	    expect( await header.payload()	).to.have.length( 16 );
 
 	    parser.stop();
 	}
