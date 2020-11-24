6798e1e1998f3de34ce7047e99099c1571d52c9b
Matthew Brisebois <matthew@webheroes.ca>
2020-11-24 10:52:07 -0700

    Move structs to their own file

--- src/types.js
+++ src/types.js
@@ -8,7 +8,6 @@ const {
     $TYPE,
     $LAIRCLIENT,
     LairClientError,
-    HEADER_SIZE,
 }					= require('./constants.js');;
 
 class ConversionError extends LairClientError {
@@ -188,128 +187,16 @@ Object.defineProperty( LairSignature, "SIZE", { value: 64, writable: false });
 
 
 
-class LairStruct extends Array {
-    [Symbol.toStringTag]		= LairStruct.name;
-
-    static from ( source, has_header_prefix = false ) {
-	source				= Buffer.from( source );
-	let struct			= new this();
-
-	if ( has_header_prefix === true )
-	    source			= source.slice( 16 );
-
-	for (let i=0; i < this.PAYLOAD.length; i++) {
-	    let type			= this.PAYLOAD[i];
-	    let value			= type.fromSource( source );
-	    source			= source.slice( value.length );
-
-	    struct.push( value );
-	}
-
-	if ( source.length > 0 )
-	    log.warn("source (%s) for struct %s still has some bytes leftover; this may be unexpected.", source.length, struct.name );
-
-	return struct;
-    }
-
-    constructor ( ...args ) {
-	super();
-
-	if ( this.constructor.PAYLOAD.length < args.length )
-	    throw new ConversionError(`Received more arguments (${args.length}) than template allows for ${this.name}`);
-
-	this.byteLength			= HEADER_SIZE;
-	for (let i=0; i < args.length; i++) {
-	    let type			= this.constructor.PAYLOAD[i];
-	    let value			= args[i] === undefined
-		? new LairType(0)
-		: type.from( args[i] );
-
-	    this.push( value );
-	}
-    }
-
-    push ( value ) {
-	if ( !(value instanceof LairType) )
-	    throw new TypeError(`Structs can only contain LairType objects, not '${value.constructor.name}'`);
-
-	if ( this.length === this.constructor.PAYLOAD.length )
-	    throw new Error(`Cannot push another item onto ${this.constructor.name} because it already contains the expected number of payoad items.`);
-
-	this.byteLength		       += value.length;
-	return super.push( ...arguments );
-    }
-
-    toMessage ( msg_id ) {
-	if (msg_id === undefined) {
-	    if ( this instanceof LairResponse )
-		throw new TypeError(`Message ID must be provided for Lair responses`);
-	    else
-		msg_id			= Math.floor(Math.random() * 2**32);
-	}
-
-	let buf				= new LairType( this.byteLength );
-
-	buf.view.writeUInt32LE( this.byteLength );
-	buf.view.writeUInt32LE( this.constructor.WIRE_TYPE,	4 );
-	buf.view.writeBigUInt64LE( BigInt(msg_id),		8 );
-
-	let position			= HEADER_SIZE;
-	for (let value of this) {
-	    buf.set( value, position );
-	    position		       += value.length;
-	}
-
-	return buf;
-    }
-
-    value ( index ) {
-	let type			= this[index];
-	if ( type instanceof LairType )
-	    return type.value();
-	else
-	    return null;
-    }
-}
-
-class LairRequest extends LairStruct {
-    isRequest () { return true; }
-    isResponse () { return false; }
-};
-class LairResponse extends LairStruct {
-    isRequest () { return false; }
-    isResponse () { return true; }
-};
-
-class UnlockPassphraseRequest extends LairRequest {
-    static WIRE_TYPE			= 4278190096;
-    static PAYLOAD			= [];
-}
-class UnlockPassphraseResponse extends LairResponse {
-    static WIRE_TYPE			= 4278190097;
-    static PAYLOAD			= [ LairString ];
-}
-
-class SignByPublicKeyRequest extends LairRequest {
-    static WIRE_TYPE			= 576;
-    static PAYLOAD			= [ LairPublicKey, LairSized ];
-}
-class SignByPublicKeyResponse extends LairResponse {
-    static WIRE_TYPE			= 577;
-    static PAYLOAD			= [ LairSignature ];
-}
-
-
 
 module.exports = {
     LairType,
+
+    // Uint8Array types
     LairString,
     LairSized,
-    LairStruct,
-    UnlockPassphraseRequest,
-    UnlockPassphraseResponse,
-    SignByPublicKeyRequest,
-    SignByPublicKeyResponse,
-    LairClientError,
+    LairPublicKey,
+    LairSignature,
+
+    // Error types
     ConversionError,
 };
--- tests/unit/test_types.js
+++ tests/unit/test_types.js
@@ -7,11 +7,6 @@ const expect				= require('chai').expect;
 const { LairType,
 	LairString,
 	LairSized,
-	LairStruct,
-	UnlockPassphraseRequest,
-	UnlockPassphraseResponse,
-	SignByPublicKeyRequest,
-	SignByPublicKeyResponse,
 	ConversionError }		= require('../../src/types.js');
 
 const compress				= str => str.replace(/ /g, "");
@@ -74,59 +69,6 @@ function types_tests () {
 	    bytes[0]			= 1;
 	    bytes.toType( LairSized );
 	}).to.throw("Too many bytes: LairSized", ConversionError);
-    });
-
-    it("should encode/decode wire type 'Unlock Passphrase Response (4278190097)'", async () => {
-	let passphrase			= "Passw0rd!";
-	let wiretype			= new UnlockPassphraseResponse( passphrase );
-	expect( wiretype		).to.be.a("LairStruct");
-	expect( wiretype.isResponse()	).to.be.true;
-	expect( wiretype instanceof UnlockPassphraseResponse ).to.be.true;
-	expect( wiretype.byteLength	).to.equal( 33 );
-
-	let bytes			= wiretype.toMessage( 1 );
-	expect( bytes.toHex()		).to.equal( compress("21 00 00 00 11 00 00 ff 01 00 00 00 00 00 00 00 09 00 00 00 00 00 00 00 50 61 73 73 77 30 72 64 21") );
-
-
-	let struct			= UnlockPassphraseResponse.from( bytes, true );;
-
-	expect( struct.byteLength	).to.equal( 33 );
-	expect( struct[0]		).to.be.a("LairType");
-	expect( struct.value(0)		).to.equal( passphrase );
-
-	expect(() => {
-	    UnlockPassphraseResponse.from( bytes.slice( 16, 20 ) );
-	}).to.throw("does not have enough bytes to get LairString size", ConversionError);
-
-	expect(() => {
-	    UnlockPassphraseResponse.from( bytes.slice( 16, 26 ) );
-	}).to.throw("Not enough bytes: LairString", ConversionError);
-    });
-
-    it("should encode/decode wire type 'Sign by Public Key Request (576)'", async () => {
-	let pub_key			= Buffer.from( "P/rh2HWYa2u6wD6yd+7lBfw2yjAilo9m+0EsS0d9xRw=", "base64" );
-	let message			= Buffer.from( compress("05 54 89 8a 56 b4 ff 4e 83 be 46 5c d6 4d 0f c9 12 79 04 a0 5a aa e7 b6 45 cb fc c1 91 3b 1c d3 87 75 2b 48 24 11 4b c1"), "hex" );
-	let wiretype			= new SignByPublicKeyRequest( pub_key, message );
-	expect( wiretype		).to.be.a("LairStruct");
-	expect( wiretype.isRequest()	).to.be.true;
-	expect( wiretype instanceof SignByPublicKeyRequest ).to.be.true;
-	expect( wiretype.byteLength	).to.equal( 96 );
-
-	let bytes			= wiretype.toMessage( 1 );
-	expect( bytes.toHex()		).to.equal( "600000004002000001000000000000003ffae1d875986b6bbac03eb277eee505fc36ca3022968f66fb412c4b477dc51c28000000000000000554898a56b4ff4e83be465cd64d0fc9127904a05aaae7b645cbfcc1913b1cd387752b4824114bc1" );
-
-
-	let struct			= SignByPublicKeyRequest.from( bytes, true );;
-
-	expect( struct.byteLength		).to.equal( 96 );
-	expect( struct[0].toHex()		).to.equal( pub_key.toString("hex") );
-	expect( struct[1].toHex()		).to.equal( "2800000000000000" + message.toString("hex") );
-	expect( struct.value(0).toString("hex")	).to.equal( pub_key.toString("hex") );
-	expect( struct.value(1).toString("hex")	).to.equal( message.toString("hex") );
-
-	expect(() => {
-	    struct.push( new LairType(0) );
-	}).to.throw("Cannot push another item onto SignByPublicKeyRequest", Error);
     });
 }
 
